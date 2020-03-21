87f19b357544466409c4ee2a8f065375fdceddbf
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-21 12:07:53 +0800

    first blood

---
+++ main.py
@@ -0,0 +1,18 @@
+from moviepy.editor import *
+from moviepy.video.tools.subtitles import SubtitlesClip
+
+from notify import send_email
+
+generator = lambda txt: TextClip(txt, font='Arial', fontsize=16, color='white')
+subtitles = SubtitlesClip("Wow! Trump Makes Coronavirus Racist - The Daily Show.en.srt", generator)
+
+video = VideoFileClip("Wow! Trump Makes Coronavirus Racist - The Daily Show.mp4")
+result = CompositeVideoClip([video, subtitles.set_position(('center','bottom'))])
+
+result.write_videofile("out.mp4")
+
+send_email(from_address=os.getenv('GOOGLE_EMAIL_USERNAME', ''),
+           to_address=os.getenv('GOOGLE_NOTIFIED_EMAIL', ''),
+           subject='Caption',
+           text='Done!',
+           attachments=['out.mp4'])
