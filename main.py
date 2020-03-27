aa750cd740dc883af5e6c8ddb4fceaca3515e871
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-27 22:42:22 +0800

    build artifacts

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 
-from notify import send_email
-
 generator = lambda txt: TextClip(txt, font='./fonts/GothamMedium.ttf', fontsize=35, color='white', bg_color='#00000066')
 subtitles = SubtitlesClip("Cleaning for Coronavirus - The Daily Show.en-US.srt", generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
 
@@ -15,9 +13,3 @@ result.write_videofile("out.mp4",
                        temp_audiofile="temp-audio.m4a",
                        remove_temp=True,
                        audio_codec="aac")
-
-send_email(from_address=os.getenv('GOOGLE_EMAIL_USERNAME', ''),
-           to_address=os.getenv('GOOGLE_NOTIFIED_EMAIL', ''),
-           subject='Caption',
-           text='Done!',
-           attachments=['out.mp4'])
