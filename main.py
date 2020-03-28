e920c84a071dbb42faa80bfc3130680d2d6b4710
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-28 09:21:56 +0800

    try another video

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 
 generator = lambda txt: TextClip(txt, font='./fonts/GothamMedium.ttf', fontsize=35, color='white', bg_color='#00000066')
-subtitles = SubtitlesClip("Cleaning for Coronavirus - The Daily Show.en-US.srt", generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
+subtitles = SubtitlesClip("States Enforce Quarantines to Discourage Travel - The Daily Social Distancing Sh.en-US.srt", generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
 
-video = VideoFileClip("Cleaning for Coronavirus - The Daily Show.mp4", audio=True)
+video = VideoFileClip("States Enforce Quarantines to Discourage Travel - The Daily Social Distancing Sh.mp4", audio=True)
 result = CompositeVideoClip([video, subtitles])
 
 result.write_videofile("out.mp4",
