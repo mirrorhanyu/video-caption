5c6521a522d05936070b2ac356f6ef4bd50ff3d6
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-28 09:44:14 +0800

    try webm file

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 
 generator = lambda txt: TextClip(txt, font='./fonts/GothamMedium.ttf', fontsize=35, color='white', bg_color='#00000066')
-subtitles = SubtitlesClip("States Enforce Quarantines to Discourage Travel - The Daily Social Distancing Sh.en-US.srt", generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
+subtitles = SubtitlesClip("Remembering When Coronavirus Was Contained - The Daily Show.en.srt", generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
 
-video = VideoFileClip("States Enforce Quarantines to Discourage Travel - The Daily Social Distancing Sh.mp4", audio=True)
+video = VideoFileClip("Remembering When Coronavirus Was Contained - The Daily Show.webm", audio=True)
 result = CompositeVideoClip([video, subtitles])
 
 result.write_videofile("out.mp4",
