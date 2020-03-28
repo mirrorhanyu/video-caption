f6186a38722a32f2cfb69fa026d5809b750e6d80
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-28 22:47:56 +0800

    test bypy

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from moviepy.editor import *
-from moviepy.video.tools.subtitles import SubtitlesClip
+# from moviepy.editor import *
+# from moviepy.video.tools.subtitles import SubtitlesClip
+#
+# generator = lambda txt: TextClip(txt, font='./fonts/GothamMedium.ttf', fontsize=35, color='white', bg_color='#00000066')
+# subtitles = SubtitlesClip("Remembering When Coronavirus Was Contained - The Daily Show.en.srt", generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
+#
+# video = VideoFileClip("Remembering When Coronavirus Was Contained - The Daily Show.webm", audio=True)
+# result = CompositeVideoClip([video, subtitles])
+#
+# result.write_videofile("out.mp4",
+#                        threads=4,
+#                        fps=video.fps,
+#                        temp_audiofile="temp-audio.m4a",
+#                        remove_temp=True,
+#                        audio_codec="aac")
 
-generator = lambda txt: TextClip(txt, font='./fonts/GothamMedium.ttf', fontsize=35, color='white', bg_color='#00000066')
-subtitles = SubtitlesClip("Remembering When Coronavirus Was Contained - The Daily Show.en.srt", generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
-
-video = VideoFileClip("Remembering When Coronavirus Was Contained - The Daily Show.webm", audio=True)
-result = CompositeVideoClip([video, subtitles])
-
-result.write_videofile("out.mp4",
-                       threads=4,
-                       fps=video.fps,
-                       temp_audiofile="temp-audio.m4a",
-                       remove_temp=True,
-                       audio_codec="aac")
+from bypy import ByPy
+bp=ByPy()
+bp.list()
