ff10eb68b24a2939e517641789fa4f86a6de9d93
mirrorhanyu <mirrorhanyu@gmail.com>
2020-04-07 20:37:38 +0800

    reduce

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.video.fx.margin import margin
 
-generator = lambda txt: TextClip(txt, font='./fonts/GothamMedium.ttf', fontsize=45, color='white', bg_color='#00000066')
-subtitles = margin(SubtitlesClip("Remembering When Coronavirus Was Contained - The Daily Show.en.srt", generator).set_position('center', 'bottom'), bottom=45, opacity=0)
+generator = lambda txt: TextClip(txt,
+                                 font='./fonts/GothamMedium.ttf',
+                                 fontsize=45,
+                                 color='white',
+                                 bg_color='#00000066')
+subtitles = margin(clip=SubtitlesClip("Remembering When Coronavirus Was Contained - The Daily Show.en.srt", generator).set_position('center', 'bottom'),
+                   bottom=35,
+                   opacity=0)
 
 video = VideoFileClip("Remembering When Coronavirus Was Contained - The Daily Show.webm", audio=True)
 result = CompositeVideoClip([video, subtitles])
