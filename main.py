9f1646c22e3e087b57563b5d6abe4dad0ff1359b
mirrorhanyu <mirrorhanyu@gmail.com>
2020-04-07 20:18:50 +0800

    try

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-# from moviepy.editor import *
-# from moviepy.video.tools.subtitles import SubtitlesClip
-#
-# generator = lambda txt: TextClip(txt, font='./fonts/GothamMedium.ttf', fontsize=35, color='white', bg_color='#00000066')
-# subtitles = SubtitlesClip("Remembering When Coronavirus Was Contained - The Daily Show.en.srt", generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
-#
-# video = VideoFileClip("Remembering When Coronavirus Was Contained - The Daily Show.webm", audio=True)
-# result = CompositeVideoClip([video, subtitles])
-#
-# result.write_videofile("out.mp4",
-#                        threads=4,
-#                        fps=video.fps,
-#                        temp_audiofile="temp-audio.m4a",
-#                        remove_temp=True,
-#                        audio_codec="aac")
-import os
+from moviepy.editor import *
+from moviepy.video.tools.subtitles import SubtitlesClip
+from moviepy.video.fx.margin import margin
 
-from bilibiliupload import *
+generator = lambda txt: TextClip(txt, font='./fonts/GothamMedium.ttf', fontsize=45, color='white', bg_color='#00000066')
+subtitles = margin(SubtitlesClip("Remembering When Coronavirus Was Contained - The Daily Show.en.srt", generator).set_position('center', 'bottom'), bottom=45, opacity=0)
 
-b = Bilibili()
-print(b.login(os.getenv('BILIBILI_USERNAME', ''), os.getenv('BILIBILI_PASSWORD', '')))
+video = VideoFileClip("Remembering When Coronavirus Was Contained - The Daily Show.webm", audio=True)
+result = CompositeVideoClip([video, subtitles])
+
+result.write_videofile("out.mp4",
+                       threads=2,
+                       fps=video.fps)
